20c0a1580de27dfca152cba2daef689ce82451f2
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-12 15:22:20 -0400

    Index/Page/template-home.php upload

--- page.php
+++ page.php
@@ -1,17 +1,16 @@
-<?php
+<?php 
 /**
- * The main template file
+ * The template for displaying all pages
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package 64Kitchen
+ * @package 64 Kitchen
  */
-
 get_header();
 ?>
 		<div class="content-area">
@@ -21,13 +20,13 @@ get_header();
 						<div class="row">
 							<?php
 								// If there are any posts 
-								if ( have_posts() ):
+								if( have_posts() ):
 
 									// Load posts loop
 									while( have_posts() ): the_post();
 										?>
 											<article>
-												<h2><?php the_title(); ?></h2>
+												<h1><?php the_title(); ?></h1>
 												<div><?php the_content(); ?></div>
 											</article>
 										<?php
--- template-home.php
+++ template-home.php
@@ -1,27 +1,38 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package 64Kitchen
- */
+<?php 
+/*
+Template Name: Home Page
+*/ 
 
 get_header();
 ?>
 		<div class="content-area">
 			<main>
+				<section class="slider">
+					<div class="container">
+						<div class="row">Slider</div>
+					</div>
+				</section>
+				<section class="popular-products">
+					<div class="container">
+						<div class="row">Popular Products</div>
+					</div>
+				</section>
+				<section class="new-arrivals">
+					<div class="container">
+						<div class="row">New Arrivals</div>
+					</div>
+				</section>
+				<section class="deal-of-the-week">
+					<div class="container">
+						<div class="row">Deal of the Week</div>
+					</div>
+				</section>
 				<section class="lab-blog">
 					<div class="container">
 						<div class="row">
 							<?php
 								// If there are any posts 
-								if ( have_posts() ):
+								if( have_posts() ):
 
 									// Load posts loop
 									while( have_posts() ): the_post();
