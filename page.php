6f82538e886b50ec1125c11efb21e92bc3209b8f
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-13 12:05:50 -0400

    Adding full col width to cart page
    page.php col class added to article tag

--- page.php
+++ page.php
@@ -25,7 +25,7 @@ get_header();
 									// Load posts loop
 									while( have_posts() ): the_post();
 										?>
-											<article>
+											<article class="col">
 												<h1><?php the_title(); ?></h1>
 												<div><?php the_content(); ?></div>
 											</article>
