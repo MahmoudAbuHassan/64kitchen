141fd71b26701259dcdda0f650bb99c0eea824d7
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-13 16:28:07 -0400

    Removed postcode, company, and state fields from checkout page.

--- inc/wc-modifications.php
+++ inc/wc-modifications.php
@@ -50,3 +50,23 @@ function sixty4kitchen_wc_modify(){
     add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt', 1 );
 }
 add_action( 'wp', 'sixty4kitchen_wc_modify' );
+
+
+//Removing postal codes
+add_filter( 'woocommerce_checkout_fields' , 'sixty4kitchen_remove_billing_postcode_checkout' );
+ 
+function sixty4kitchen_remove_billing_postcode_checkout( $fields ) {
+  unset($fields['billing']['billing_postcode']);
+  unset($fields['billing']['billing_company']);
+  unset($fields['billing']['billing_state']);
+  return $fields;
+}
+
+
+// filtering subtotals
+add_filter( 'woocommerce_get_order_item_totals', 'remove_subtotal_from_orders_total_lines', 100, 1 );
+
+function remove_subtotal_from_orders_total_lines( $totals ) {
+    unset($totals['cart_subtotal']  );
+    return $totals;
+}
