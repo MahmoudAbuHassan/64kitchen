51b70a35ee2c6e106b5e53f17a5a572911c7dffa
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-14 09:52:42 -0400

    Removed unneeded code from functions.php
    Navwalker

--- functions.php
+++ functions.php
@@ -11,10 +11,7 @@
 /**
  * Register Custom Navigation Walker
  */
-function register_navwalker(){
 	require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
-}
-add_action( 'after_setup_theme', 'register_navwalker' );
 
 /**
 * Enqueue scripts and styles.
