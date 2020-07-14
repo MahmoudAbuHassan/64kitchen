8d564694879c2bfb88ad20bbc5e84ef39c6e2b2c
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-14 08:13:14 -0400

    Customizing main menu

--- header.php
+++ header.php
@@ -28,27 +28,31 @@
 			<section class="top-bar">
 				<div class="container">
 					<div class="row">
-						<div class="brand col-3">Logo</div>
-						<div class="second-column col-9">
-							<div class="account">Account</div>
-								<nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
-										<!-- Brand and toggle get grouped for better mobile display -->
-										<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" 	aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-											<span class="navbar-toggler-icon"></span>
-										</button>
-										<?php
-											wp_nav_menu( array(
-												'theme_location'    => 'sixty4kitchen_main_menu',
-												'depth'             => 3,
-												'container'         => 'div',
-												'container_class'   => 'collapse navbar-collapse',
-												'container_id'      => 'bs-example-navbar-collapse-1',
-												'menu_class'        => 'nav navbar-nav',
-												'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-												'walker'            => new WP_Bootstrap_Navwalker(),
-											) );
-										?>
-								</nav>
+						<div class="brand col-12 col-md-3  col-lg-2 text-center text-md-left">Logo</div>
+						<div class="second-column col-12 col-md-9 col-12 col-lg-10">
+							<div class="row">
+								<div class="account col-12">Account</div>
+								<div class="col-12">
+									<nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
+											<!-- Brand and toggle get grouped for better mobile display -->
+											<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" 	aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+												<span class="navbar-toggler-icon"></span>
+											</button>
+											<?php
+												wp_nav_menu( array(
+													'theme_location'    => 'sixty4kitchen_main_menu',
+													'depth'             => 3,
+													'container'         => 'div',
+													'container_class'   => 'collapse navbar-collapse',
+													'container_id'      => 'bs-example-navbar-collapse-1',
+													'menu_class'        => 'nav navbar-nav',
+													'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+													'walker'            => new WP_Bootstrap_Navwalker(),
+												) );
+											?>
+									</nav>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
