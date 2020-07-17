073e341ffccc290c32fc76ed74986d5e626633a4
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-17 06:26:36 -0400

    Update functions.php
    class typo

--- functions.php
+++ functions.php
@@ -76,7 +76,7 @@ function sixty4kitchen_config(){
 
     add_theme_support( 'post-thumbnails' );
     add_image_size( 'sixty4kitchen-slider', 1920, 800, array( 'center', 'center' ) );
-    add_image_size( 'sixty4kitchen_blog', 960, 640, array( 'center', 'center' ) );
+    add_image_size( 'sixty4kitchen-blog', 960, 640, array( 'center', 'center' ) );
 
     if ( ! isset( $content_width ) ) {
         $content_width = 600;
