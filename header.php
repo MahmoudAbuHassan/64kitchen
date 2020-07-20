ad4ded4e3175ad94b197bdd8ed3072ceaf63d299
MahmoudAbuHassan <64367914+MahmoudAbuHassan@users.noreply.github.com>
2020-07-20 18:54:05 -0400

    uptade header, home, single-product.

--- header.php
+++ header.php
@@ -59,10 +59,11 @@
 									</div>
 								</div>
 								<?php endif; ?>
-								<div class="col-12">
+								<!-- Navbar -->
+								<div class="col-12">  
 									<nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
 											<!-- Brand and toggle get grouped for better mobile display -->
-											<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle Navigation', '64kitchen' ) ?>">
+											<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle Navigation', '64kitchen' ) ?>">
 												<span class="navbar-toggler-icon"></span>
 											</button>
 											<?php
